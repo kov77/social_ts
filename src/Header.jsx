9216380d4d62294dd9d1b9cc3b5b7cbc7fb9e3c7
Maksym_Koval <maksim6167@gmail.com>
2022-02-24 18:05:14 -0400

    Add Header and Technologies components

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div>
+            <a>Home </a>
+            <a>About </a>
+            <a>Info </a>
+            <a>Contact</a>
+        </div>
+    )
+}
+
+export default Header;
