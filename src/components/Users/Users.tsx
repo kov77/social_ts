218dbe16d078aeb733990bb3b17db5b70cc3818f
Maksym_Koval <maksim6167@gmail.com>
2022-05-07 15:02:23 -0300

    connect to server

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,20 +1,20 @@
 import {userType} from "../../redux/users-reducer";
 import classes from './Users.module.css'
+import axios from "axios";
+import userPhoto from "../../assets/images/user_avatar.png"
 const Users = (props: any) => {
     if(props.users.length === 0) {
-        props.setUsers([
-            {id: 1, folowed: false, img: 'https://styles.redditmedia.com/t5_2qi0m/styles/communityIcon_h01scqdubtb41.png', name: 'Dmitriy', status: 'I am the boss', location: {city: 'Minsk', country: 'Belarus'}},
-            {id: 2, folowed: true, img: 'https://styles.redditmedia.com/t5_2qi0m/styles/communityIcon_h01scqdubtb41.png', name: 'Ivan', status: 'I am the poc', location: {city: 'Moscow', country: 'russia'}},
-            {id: 3, folowed: false, img: 'https://styles.redditmedia.com/t5_2qi0m/styles/communityIcon_h01scqdubtb41.png', name: 'Taras', status: 'I am the man', location: {city: 'Kyiv', country: 'Ukraine'}},
-            {id: 4, folowed: true, img: 'https://styles.redditmedia.com/t5_2qi0m/styles/communityIcon_h01scqdubtb41.png', name: 'James', status: 'I am the boy', location: {city: 'San Diego', country: 'USA'}},
-        ])
-    }
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then(response => {
+                props.setUsers([...response.data.items])
+            } )
 
+    }
     return <div>
         {
             props.users.map((user: userType) => <div className = {classes.userWrp} key={user.id}>
                 <div className={classes.profileWrp}>
-                    <div className={classes.userAvatar}><img src = {user.img} alt="user's avatar"/></div>
+                    <div className={classes.userAvatar}><img src = {user.photos.small ? user.photos.small : userPhoto} alt="user's avatar"/></div>
                     {user.folowed
                         ? <button onClick={() => {props.follow(user.id)}}>Follow</button>
                         : <button onClick={() => {props.unfollow(user.id)}}>Unfollow</button>}
@@ -23,8 +23,8 @@ const Users = (props: any) => {
                     <div className={classes.userName}>{user.name}</div>
                     <div className={classes.userStatus}>{user.status}</div>
                     <div className={classes.userLocationWrp}>
-                        <div className={classes.userCountry}>{user.location.country}</div>
-                        <div className={classes.userCity}>{user.location.city}</div>
+                        <div className={classes.userCountry}>{"user.location.country"}</div>
+                        <div className={classes.userCity}>{"user.location.city"}</div>
                     </div>
                 </div>
 
--- src/redux/users-reducer.ts
+++ src/redux/users-reducer.ts
@@ -1,21 +1,21 @@
 
 const initialState = {
-    users: [
-        // {id: 1, folowed: false, img: 'https://styles.redditmedia.com/t5_2qi0m/styles/communityIcon_h01scqdubtb41.png', name: 'Dmitriy', status: 'I am the boss', location: {city: 'Minsk', country: 'Belarus'}},
-        // {id: 2, folowed: true, img: 'https://styles.redditmedia.com/t5_2qi0m/styles/communityIcon_h01scqdubtb41.png', name: 'Ivan', status: 'I am the poc', location: {city: 'Moscow', country: 'russia'}},
-        // {id: 3, folowed: false, img: 'https://styles.redditmedia.com/t5_2qi0m/styles/communityIcon_h01scqdubtb41.png', name: 'Taras', status: 'I am the man', location: {city: 'Kyiv', country: 'Ukraine'}},
-        // {id: 4, folowed: true, img: 'https://styles.redditmedia.com/t5_2qi0m/styles/communityIcon_h01scqdubtb41.png', name: 'James', status: 'I am the boy', location: {city: 'San Diego', country: 'USA'}},
-    ]
+    users: []
 }
 type userLocationType = {
     city: string
     country: string
 }
 
+type photosType = {
+        small: string
+        large: string
+}
+
 export type userType = {
     id: number
     folowed: boolean
-    img: string
+    photos: photosType
     name: string
     status: string
     location: userLocationType
