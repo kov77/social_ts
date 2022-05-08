674583b3d1b59a6fbf444f90e5553a7aa421bd9e
Maksym_Koval <maksim6167@gmail.com>
2022-05-08 15:51:01 -0300

    add class component Users

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,35 +2,39 @@ import {userType} from "../../redux/users-reducer";
 import classes from './Users.module.css'
 import axios from "axios";
 import userPhoto from "../../assets/images/user_avatar.png"
-const Users = (props: any) => {
-    if(props.users.length === 0) {
+import React from "react";
+
+class Users extends React.Component<any, any>{
+    constructor(props: any) {
+        super(props);
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
-                props.setUsers([...response.data.items])
+                this.props.setUsers([...response.data.items])
             } )
-
     }
-    return <div>
-        {
-            props.users.map((user: userType) => <div className = {classes.userWrp} key={user.id}>
-                <div className={classes.profileWrp}>
-                    <div className={classes.userAvatar}><img src = {user.photos.small ? user.photos.small : userPhoto} alt="user's avatar"/></div>
-                    {user.folowed
-                        ? <button onClick={() => {props.follow(user.id)}}>Follow</button>
-                        : <button onClick={() => {props.unfollow(user.id)}}>Unfollow</button>}
-                </div>
-                <div className={classes.userInfoWrp}>
-                    <div className={classes.userName}>{user.name}</div>
-                    <div className={classes.userStatus}>{user.status}</div>
-                    <div className={classes.userLocationWrp}>
-                        <div className={classes.userCountry}>{"user.location.country"}</div>
-                        <div className={classes.userCity}>{"user.location.city"}</div>
+    render() {
+        return <div>
+            {
+                this.props.users.map((user: userType) => <div className = {classes.userWrp} key={user.id}>
+                    <div className={classes.profileWrp}>
+                        <div className={classes.userAvatar}><img src = {user.photos.small ? user.photos.small : userPhoto} alt="user's avatar"/></div>
+                        {user.folowed
+                            ? <button onClick={() => {this.props.follow(user.id)}}>Follow</button>
+                            : <button onClick={() => {this.props.unfollow(user.id)}}>Unfollow</button>}
                     </div>
-                </div>
+                    <div className={classes.userInfoWrp}>
+                        <div className={classes.userName}>{user.name}</div>
+                        <div className={classes.userStatus}>{user.status}</div>
+                        <div className={classes.userLocationWrp}>
+                            <div className={classes.userCountry}>{"user.location.country"}</div>
+                            <div className={classes.userCity}>{"user.location.city"}</div>
+                        </div>
+                    </div>
 
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+    }
 }
 
 export default Users
